539d1f461c289fe8f6094ab8ba2d211ae41b02fd
sean.rivera <sean.rivera@uni.lu>
2019-07-10 14:22:19 +0200

    Merge branch Added wrapper code to use perf for packet tracing

---
+++ packet_trace/packet_trace.go
@@ -0,0 +1,112 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	bpf "github.com/iovisor/gobpf/bcc"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	device            = "lo"
+	snapshotLen int32 = 1024
+	promiscuous       = false
+	_           error
+	timeout     = 30 * time.Second
+	handle      *pcap.Handle
+)
+
+func main() {
+	filesrc, err := ioutil.ReadFile("packet_trace.bpf")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load xdp source %v\n", err)
+		os.Exit(1)
+	}
+	source := string(filesrc)
+
+	ret := "XDP_DROP"
+	ctxtype := "xdp_md"
+
+	module := bpf.NewModule(source, []string{
+		"-w",
+		"-DRETURNCODE=" + ret,
+		"-DCTXTYPE=" + ctxtype,
+	})
+	defer module.Close()
+
+	fn, err := module.Load("xdp_prog1", C.BPF_PROG_TYPE_XDP, 1, 65536)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load xdp prog: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = module.AttachXDP(device, fn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to attach xdp prog: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if err := module.RemoveXDP(device); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to remove XDP from %s: %v\n", device, err)
+		}
+	}()
+
+	headers := bpf.NewTable(module.TableId("headers"), module)
+	header_list := headers.Iter()
+	head_size := bpf.NewTable(module.TableId("head_size"), module)
+	hs_list := head_size.Iter()
+
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		tcpLayer := packet.Layer(layers.LayerTypeTCP)
+		tcp, _ := tcpLayer.(*layers.TCP)
+		for header_list.Next() {
+			key, leaf := header_list.Key(), header_list.Leaf()
+			leaf_val, _ := headers.LeafBytesToStr(leaf)
+			if fmt.Sprint(tcp.Seq) == leaf_val {
+				applicationLayer := packet.ApplicationLayer()
+				if applicationLayer != nil {
+					fmt.Println("Application layer/Payload found.")
+					fmt.Printf("%s\n", applicationLayer.Payload())
+
+					// Search for a string inside the payload
+					if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
+						fmt.Println("HTTP found!")
+					}
+				}
+
+				// Check for errors
+				if err := packet.ErrorLayer(); err != nil {
+					fmt.Println("Error decoding some part of the packet:", err)
+				}
+				new_leaf, _ := headers.LeafStrToBytes("0")
+				headers.Set(key, new_leaf)
+				// Should only be one
+				for hs_list.Next() {
+					key, leaf := hs_list.Key(), hs_list.Leaf()
+					leaf_str, _ := headers.LeafBytesToStr(leaf)
+					leaf_int, _ := strconv.Atoi(leaf_str)
+					leaf_int = leaf_int - 1
+					new_leaf, _ := headers.LeafStrToBytes(fmt.Sprint(leaf_int))
+					head_size.Set(key, new_leaf)
+				}
+			}
+		}
+	}
+}
