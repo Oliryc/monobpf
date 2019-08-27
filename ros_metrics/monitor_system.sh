0be8ef07aeb537c00073b236e269cde174b3b651
Sean Rivera <sean.au.rivera@gmail.com>
2019-08-27 11:33:39 +0200

    More metrics >.<

--- ros_metrics/monitor_system.sh
+++ ros_metrics/monitor_system.sh
@@ -1,4 +1,12 @@
 #!/bin/bash 
-export COUNTER=1
-perf stat -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./exec_metrics.sh  
-
+#export COUNTER=1
+mkdir results
+for COUNTER in {1..15}
+do
+    for NODES in 1 5 10 20 40 60 80
+    do
+    	echo "$COUNTER + $NODES"
+    	perf stat -o results/expirement1.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./exec_metrics.sh $NODES 2>&1
+    
+    done
+done
