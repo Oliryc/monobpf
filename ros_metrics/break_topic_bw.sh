e47cccc48bce586e4896178a483b3157661401bd
Sean Rivera <sean.au.rivera@gmail.com>
2019-09-19 16:39:31 +0200

    Updates?

---
+++ ros_metrics/break_topic_bw.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+source /opt/ros/melodic/setup.bash
+export TURTLEBOT3_MODEL=waffle
+export EDITOR=/usr/bin/vim
+export PCP_PMDAS_DIR=/var/lib/pcp/pmdas/
+export PCP_LOG_DIR=/var/log/pcp/pmcd/
+export PCP_DIR=/
+export PCP_SHARE_DIR=/usr/share/pcp/
+roscore 1>/dev/null &
+export CORE_PID=$!
+echo "ROSCORE STARTED WITH PID $CORE_PID"
+sleep 5
+argv=("$@")
+node=${argv[0]}
+rate=${argv[1]}
+echo "Launching $node nodes"
+for ((i=1; i<=$node; i++))
+do
+rosrun rospy_tutorials talker.py 1>/dev/null &
+TALK_PID[$i]=$!
+rosrun rospy_tutorials listener.py 1>/dev/null &
+#roslaunch rospy_tutorials talker_listener.launch 1>/dev/null &
+LISTEN_PID[$i]=$!
+done
+export LAUNCH_PID
+echo "Talker STARTED WITH PID ${TALK_PID[@]}"
+echo "Listener STARTED WITH PID ${LISTEN_PID[@]}"
+sleep 5
+echo "rate is $rate"
+rostopic bw -w $rate /chatter &
+export APP1_PID=$!
+#rostopic delay -w $rate /chatter &
+#export APP2_PID=$!
+#rostopic hz -w $rate /chatter &
+#export APP3_PID=$!
+echo "PROCESS STARTED WITH PID $APP_PID"
+sleep 100
+kill -2 $APP1_PID
+kill -2 $APP2_PID
+kill -2 $APP3_PID
+for ((i=1; i<=$node; i++))
+do
+kill -2 ${TALK_PID[$i]}
+kill -2 ${LISTEN_PID[$i]}
+done
+kill -2 ${TALK_PID[@]}
+kill -2 ${LISTEN_PID[@]}
+kill -2 $CORE_PID
+sleep 5
+kill -9 $APP1_PID
+kill -9 $APP2_PID
+kill -9 $APP3_PID
+for ((i=1; i<=$node; i++))
+do
+kill -9 $TALK_PID[$i]
+kill -9 $LISTEN_PID[$i]
+done
+kill -9 $CORE_PID
+kill -9 ${TALK_PID[@]}
+kill -9 ${LISTEN_PID[@]}
+
+rosclean purge -y
--- ros_metrics/find_breakeven.sh
+++ ros_metrics/find_breakeven.sh
@@ -1,17 +1,19 @@
 #!/bin/bash 
 #export COUNTER=1
 mkdir results
+export NODES=60
+
 for COUNTER in {1..15}
 do
-    for NODES in 1 5 10 20 40 60 80
+    for SAMPLES in 1 5 10 50 100 200 500 1000
     do
     	echo "$COUNTER + $NODES"
     	#perf stat -o results/control.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./control.sh $NODES 2>&1
     	#perf stat -o results/expirement1.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./exec_metrics.sh $NODES 2>&1
-    	#perf stat -o results/topic_bw.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./get_topic_bw.sh $NODES 2>&1
+    	perf stat -o results/break_bw.res$COUNTER.node$SAMPLES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./break_topic_bw.sh $NODES $SAMPLES 2>&1
     	#perf stat -o results/topic_delay.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./get_topic_delay.sh $NODES 2>&1
-    	perf stat -o results/topic_hz.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./get_topic_hz.sh $NODES 2>&1
-    	perf stat -o results/topic_all.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./get_topic_all.sh $NODES 2>&1
+    	#perf stat -o results/topic_hz.res$COUNTER.node$NODES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./get_topic_hz.sh $NODES 2>&1
+    	perf stat -o results/break_all.res$COUNTER.node$SAMPLES -e cycles,instructions,cache-references,cache-misses,bus-cycles -d ./break_topic_all.sh $NODES $SAMPLES 2>&1
    	killall -9 roscore rosout rosmaster python2 
     done
 done
